f5d700eae784c967af06bff57623fa932883a99c
Lander Joshua <lander.vitug@gmail.com>
2021-04-03 17:46:35 -0500

    Optimized, cleaned up, ensured that ship movement was working. Restructured the data storage and draw function. Lots of quality of life improvements

--- game.asm
+++ game.asm
@@ -36,19 +36,26 @@
 
 .data
 # structure of every obj
-# [obj]		.word		[color] [pos] [x-bound] [y-bound] [num_pixels (size of array)]
+# [obj]		.word		[pos] [size of offset_array (in bytes)] [address of offset_array (initialized)]
 # [obj_offset]	.half		[array of offsets relative to obj.pos]
+# NOTE that the color of the ship is made explicit in the .eqv
+# NOTE that the bounds of each obj are made explicit and hardcoded in the boundary and collision check (specified in comments)
 
-ship:		.word		0x4a4a4a 4160 1 1 10
-ship_offset:	.half		-260 0 4 252 256
+# ship has: (1, 1) bounds relative to the center of the ship
+ship:			.word		4160 10 0
+ship_offset:		.half		-260 0 4 252 256
 
 .text
 .eqv BASE_ADDRESS 0x10008000
+.eqv BG_COLOR 0xfcfcfc
+
 .eqv KEYSTROKE 0xffff0000
+
 .eqv WIDTH 64
 .eqv HEIGHT 32
 
-.eqv MV_SP 1				# this is for later when we want to increase the move speed of our ship (this is the number of bits to inc speed!)
+.eqv SHIP_COLOR	0x4a4a4a
+
 
 # FIX THE COMMENTS
 # FIX THE BUGS
@@ -63,7 +70,7 @@ main:
 	# $s0=base_addr; $s1=canvas_color; 
 	# $s2=row_i; $s3=col_i; $s4=A[i][j] (where A is the framebuffer)
 	li $s0, BASE_ADDRESS		# set starting address
-	li $s1, 0xfcfcfc		# set color of the canvas
+	li $s1, BG_COLOR		# set color of the canvas
 	addi $s2, $zero, 0		# set starting index (for row_loop)
 row_loop:
 	addi $s3, $zero, 0		# set starting index (for col_loop)
@@ -93,134 +100,115 @@ e_cl:	bne $s3, 256, col_loop		# if address not yet at end of framebuffer, jump t
 	addi $s2, $s2, 1		# increment the index
 e_rl:	bne $s2, 32, row_loop		# if index not yet 32, jump to row_loop
 	
-	# draw the ship
-	# $s0=&ship; $s1=&ship_offset; $s2=obj.color
-	# $t0=color; $t1=pos; $t2=num_pixels; $t3=&obj_offset; 
-	addi $sp, $sp, -16
-	la $s0, ship			# get &obj
-	la $s1, ship_offset		# get &obj_offset
-	lw $s2, 0($s0)			# get obj.color
-	lw $s3, 4($s0)			# get obj.pos
-	lw $s4, 16($s0)			# get obj.num_pixels
+	# initialize AND draw the ship
+	# $s0=&ship; $s1=temp; $s2=temp;
+	addi $sp, $sp, -12
+	la $s0, ship			# get address of ship
+	la $s2, ship_offset		
+	sw $s2, 8($s0)			# assign address of ship_offset in ship
+	lw $s2, 0($s0)			# get pos of ship
+	li $s1, SHIP_COLOR		# get color of ship
 	
-	sw $s2, 0($sp)			# push &obj
-	sw $s3, 4($sp)			# push &obj_offset
-	sw $s4, 8($sp)			# push obj.color
-	sw $s1, 12($sp)
-	
+	sw $s0, 0($sp)			# push &ship
+	sw $s1, 4($sp)			# push color
+	sw $s2, 8($sp)			# push pos
 	jal draw			# call 'draw' function
 	
 # START-SCREEN
 
 # GAME-LOOP
-# $s0=&ship; $s1=&ship_offset; $s2=&obj_arr
+# $s0=&ship; $s1=&obj_arr
 game_loop:
 	
-get_input:
-	# get input AND verify ship.pos
-	# $s3=keystroke_val; $s4=temp; $s5=old-ship.pos; $s6=new-ship.pos; $s7=relevant_bound;
-	# $t0=old-x-coord; $t1=old-y-coord; $t2=new-x-coord; $t3=new-y-coord;
+update_ship:
+	# get input
+	# $s3=keystroke_val;
 	li $s3, KEYSTROKE
 	lw $s3, 0($s3)			# get keystroke-event value
-	bne $s3, 1, e_i			# ensure that user did input something, otherwise jump to e_i
+	bne $s3, 1, e_ship		# ensure that user did input something, otherwise jump to e_i
 	li $s3, KEYSTROKE
 	lw $s3, 4($s3)			# get ASCII user input
 	
-	lw $s5, 4($s0)			# get ship.pos
+	### SOMETHING IS GOING ON WITH THE X-Y COORDS
+	
+	# store old-x-y-coords
+	# $s0=&ship; $s5=old-ship.pos; $t0=old-x-coord; $t1=old-y-coord; $t2=new-x-coord; $t3=new-y-coord;
+	lw $s5, 0($s0)			# get ship.pos
 	srl $t1, $s5, 8			# calc y-coord=(old-ship.pos / 2^8)
 	sll $s4, $t1, 6			# calc (y-coord * 2^6)
 	srl $t0, $s5, 2			# calc (old-ship.pos / 2^2)
-	sub $t0, $t0, $t1		# calc x-coord=(old-ship.pos / 2^2) - (y-coord * 2^6)
+	sub $t0, $t0, $s4		# calc x-coord=(old-ship.pos / 2^2) - (y-coord * 2^6)
 	
-	li $s4, WIDTH
-	sll $s4, $s4, 2			# calc WIDTH * 4
-	# get new ship.pos
-	sub $s6, $s5, $s4		# new pos is (addr - W)
-	# lw $s7, 12($s0)			# set relevant_bound to y-bound
-	subi $t3, $t1, 2		# update y-coord to the new pos
-	addi $t2, $t0, 0		# update x-coord to the new pos
-	beq $s3, 119, c_i		# input was UP, check it's still valid
-	
-	add $s6, $s5, $s4		# new pos is (addr + W)
-	# lw $s7, 12($s0)			# set relevant_bound to y-bound
-	addi $t3, $t1, 2		# update y-coord to the new pos
-	addi $t2, $t0, 0		# update x-coord to the new pos
-	beq $s3, 115, c_i		# input was DOWN, check it's still valid 
-	
-	addi $s6, $s5, 1		# new pos is (addr + 1)
-	# lw $s7, 8($s0)			# set relevant_bound to x-bound
-	addi $t2, $t0, 2		# update x-coord to the new pos
-	addi $t3, $t1, 0		# update y-coord to the new pos
-	beq $s3, 97, c_i		# input was LEFT, check it's still valid
-	
-	subi $s6, $s5, 1		# new pos is (addr - 1)
-	# lw $s7, 8($s0)			# set relevant_bound to x-bound
-	subi $t2, $t0, 2		# update x-coord to the new pos
-	addi $t3, $t1, 0		# update y-coord to the new pos
-	beq $s3, 100, c_i		# input was RIGHT, check it's still valid
-	
-	# we know what our ship is, we can hard-code and save some registers
-	
-	j e_i				# input was NONE OF THE ABOVE
-c_i:
-	# check that new-pos is within bounds of our framebuffer
-	
-	sw $s6, 4($s0)			# set ship.pos to new pos
-	
-	bge $t2, HEIGHT, r1		# check if new-x-coord is within range of framebuffer
-	blt $t2, 0, r2
-	bge $t3, WIDTH, r3		# check if new-y-coord is within range of framebuffer
-	blt $t3, 0, r4
-	j e_i				# input is VALID
+	add $t2, $zero, $t0		# copy old-coords onto the new-coords
+	add $t3, $zero, $t1
 
-r1:
-	addi $t2, $zero, 31		# updated to be on edge
-	j e_r
-r2:
-	add $t2, $zero, $zero		# updated to be on edge
-	j e_r
-r3:
-	addi $t3, $zero, 63		# updated to be on edge
-	j e_r
-r4:
-	add $t3, $zero, $zero		# updated to be on edge
-e_r:
-	sll $s6, $t3, 6
-	add $s6, $s6, $t2		
-	sll $s6, $s6, 2			# convert x-y coords into addressable index
-	sw $s6, 4($s0)			# set ship.pos to valid new pos
-e_i:
+	# check to ensure input was valid
+	# $s3=keystroke_val; $t0=old-x-coord; $t1=old-y-coord; $t2=new-x-coord; $t3=new-y-coord;
+	beq $s3, 119, W			# input was W
+	beq $s3, 115, S			# input was S
+	beq $s3, 97, A			# input was A
+	beq $s3, 100, D			# input was D
+	j e_ship			# input was INVALID
+
+W:	addi $t3, $t1, -2		# calc new-y-coord with padding 
+	blt $t3, 0, v_lb		# if invalid, go to vertical-lower-bound
+	addi $t3, $t3, 1		# remove padding
+	j s_i				
+S:	addi $t3, $t1, 2		# calc new-y-coord with padding 
+	bge $t3, HEIGHT, v_ub		# if invalid, go to vertical-upper-bound
+	addi $t3, $t3, -1		# remove padding
+	j s_i
 	
-	# redraw
-	# FOR NOW: we will just draw previous position with bg color and draw new position with obj color
-	# however we have two issues:
-	# what about when there are collisions and the obstacles are not the bg?
-	# what about when the pos hasn't changed?
-	# $s0=&ship; $s1=&ship_offset; $s2=obj.color
-	# $t0=color; $t1=pos; $t2=num_pixels; $t3=&obj_offset; 
-	addi $sp, $sp, -16
-	la $s3, 0xfcfcfc		# get canvas color
-	lw $s4, 16($s0)			# get ship.num_pixels
-	la $s6, ship_offset		# get &obj_offset
-	sw $s3, 0($sp)			# push canvas color
-	sw $s5, 4($sp)			# push old pos
-	sw $s4, 8($sp)			# push ship.num_pixels
-	sw $s6, 12($sp)			# push &obj_offset
+v_lb:	addi $t3, $zero, 1		# set new-y-coord (without padding) at 1
+	j s_i
+v_ub:	addi $t3, $zero, HEIGHT		# set new-y-coord (without padding) at HEIGHT-2
+	addi $t3, $t3, -2
+	j s_i 
+
+A:	addi $t2, $t0, -2		# calc new-x-coord with padding
+	blt $t2, 0, h_lb		# if invalid, go to horizontal-lower-bound
+	addi $t2, $t2, 1		# remove padding
+	j s_i
+D:	addi $t2, $t0, 2		# calc new-x-coord with padding
+	bge $t2, WIDTH, h_ub		# if invalid, got horizontal-upper-bound
+	addi $t2, $t2, -1		# remove padding
+	j s_i
 	
+h_lb:	addi $t2, $zero, 1		# set new-x-coord (without padding) at 1
+	j s_i
+h_ub:	addi $t2, $zero, WIDTH		# set new-x-coord (without padding) at WIDTH-2
+	addi $t2, $t2, -2
+	
+s_i:
+	# convert new-x-y coords 
+	# $s2-old-pos; $s3=new-pos; $s4=temp; $t2=new-x-coord; $t3=new-y-coord;
+	sll $t3, $t3, 6			# calc (new-y-coord * 2^6)
+	add $s3, $t3, $t2		# calc (new-y-coord * 2^6) + new-x-coord
+	sll $s3, $s3, 2			# calc ((new-y-coord * 2^6) + new-x-coord) * 2^2
+	
+	# with differences, set it AND redraw it (otherwise, don't redraw)
+	# $s0=&ship; $s1=temp; $s2=temp;
+	addi $sp, $sp, -12
+	la $s0, ship			# get address of ship
+	li $s1, BG_COLOR		# get color of canvas
+	
+	sw $s0, 0($sp)			# push &ship
+	sw $s1, 4($sp)			# push color
+	sw $s5, 8($sp)			# push pos
 	jal draw			# call 'draw' function
 	
-	# $s0=&ship; $s1=&ship_offset; $s2=obj.color
-	addi $sp, $sp, -16
-	lw $s3, 0($s0)			# get obj.color
-	lw $s4, 4($s0)			# get obj.pos
-	lw $s7 16($s0)			# get obj.num_pixels
-	la $s6, ship_offset		# get &ship_offset
-	sw $s3, 0($sp)			# push obj.color
-	sw $s4, 4($sp)			# push new pos
-	sw $s7, 8($sp)			# push ship.num_pixels
-	sw $s6, 12($sp)			# push &obj_offset
+	# $s0=&ship; $s1=temp; $s2=temp;
+	addi $sp, $sp, -12
+	la $s0, ship			# get address of ship
+	li $s1, SHIP_COLOR		# get color of ship
 	
+	sw $s0, 0($sp)			# push &ship
+	sw $s1, 4($sp)			# push color
+	sw $s3, 8($sp)			# push pos
 	jal draw			# call 'draw' function
+	
+	sw $s3, 0($s0)
+e_ship:
 	
 	# sleep
 	li $v0, 32
@@ -232,26 +220,30 @@ e_i:
 	li $v0, 10			# gracefully terminate the program (with grace)
 	syscall
 
-# FUNC PARAM	: $t0=color; $t1=pos; $t2=num_pixels; $t3=&obj_offset; 
-# LOCAL REG 	: $t5=temp; $t6=temp; $t7=temp;
+
+# FUNC PARAM	: $t0=&obj; $t1=color; $t2=pos;
+# LOCAL REG 	: $t3=&A (where A is the obj_offset array); $t4=temp;
+#		  $t5=index; $t6=pixel_addr; $t7=A[i] (where A is the obj_offset array);
 draw:
-	lw $t0, 0($sp)			# pop color
-	lw $t1, 4($sp)			# pop pos
-	lw $t2, 8($sp)			# pop num_pixels
-	lw $t3, 12($sp)			# pop &obj_offset
-	addi $sp, $sp, 16
+	lw $t0, 0($sp)			# pop &obj
+	lw $t1, 4($sp)			# pop color
+	lw $t2, 8($sp)			# pop pos
+	addi $sp, $sp, 12
+	
+	lw $t3, 8($t0)			# load address of obj_offset array from &obj
+	lw $t4, 4($t0)			# load size of obj_offset array from &obj 
 	
 	add $t5, $zero, $zero		# set index
 draw_loop:
 	add $t7, $t3, $t5
 	lh $t7, 0($t7)			# store current offset in $t7
 	
-	add $t6, $t1, $t7
+	add $t6, $t2, $t7
 	addi $t6, $t6, BASE_ADDRESS	# calc (BASE_ADDRESS + obj.pos + obj_offset[i])
-	sw $t0, 0($t6)			# paint at that address the given color
+	sw $t1, 0($t6)			# paint at that address the given color
 	
 	addi $t5, $t5, 2		# increment the index
-	bne $t5, $t2, draw_loop		# if the index is not the same as obj.num_pixels, jump back to the draw_loop
+	bne $t5, $t4, draw_loop		# if the index is not the same as obj.num_pixels, jump back to the draw_loop
 	
 	jr $ra				# return back
 
